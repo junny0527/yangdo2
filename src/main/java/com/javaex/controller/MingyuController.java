7774a6ee2b5acb291131d0d07f92c0d7eecc4fd4
minnnnnk <jubal9153@gmail.com>
2022-07-27 14:06:55 +0900

    테스트까지 함

--- src/main/java/com/javaex/controller/MingyuController.java
+++ src/main/java/com/javaex/controller/MingyuController.java
@@ -1,5 +1,15 @@
 package com.javaex.controller;
 
-public class MingyuController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class MingyuController {
+	
+	@RequestMapping(value="/yangdo",method = {RequestMethod.GET,RequestMethod.POST})
+	public String yangdo() {
+		
+		return "/yangdo/yangdo";
+	}
 }
