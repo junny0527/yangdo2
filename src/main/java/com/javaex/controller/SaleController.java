a03e182583223bff856d0f547f97a797d9629f7c
minnnnnk <jubal9153@gmail.com>
2022-08-05 12:13:09 +0900

    양도

--- src/main/java/com/javaex/controller/SaleController.java
+++ src/main/java/com/javaex/controller/SaleController.java
@@ -1,16 +1,32 @@
 package com.javaex.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.service.SaleService;
+import com.javaex.vo.ReservationVo;
 
 @Controller
 public class SaleController {
 	
+	@Autowired
+	private SaleService saleService;
+	
 	@RequestMapping(value="/sale",method = {RequestMethod.GET,RequestMethod.POST})
-	public String sale() {
-		
+	public String sale(@RequestParam("no") int no,Model model) {
 		System.out.println("sale");
+		System.out.println(no);
+		Map<String,Object> sMap = saleService.getReservation(no);
+		
+		model.addAttribute("sMap", sMap);
+		
+		
 		return "/sale/yangdo";
 	}
 }
