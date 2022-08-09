f9838824930cadf0fd75ea5d83af9653aeee6d05
Mocking_jayk0918 <jesper2able@gmail.com>
2022-08-10 00:03:17 +0900

    업주 업체등록폼 체크박스 제외 완료(업체 등록폼 진행도 80%)

--- src/main/java/com/javaex/controller/HostCompanyregController.java
+++ src/main/java/com/javaex/controller/HostCompanyregController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.service.HostCompanyregService;
 import com.javaex.vo.HostCompanyregVo;
@@ -26,10 +27,32 @@ public class HostCompanyregController {
 		model.addAttribute("cVo", cVo);
 		return "/host/companyRegister";
 	}
-	/*
+
 	@RequestMapping(value="companysave", method = {RequestMethod.GET, RequestMethod.POST})
-	public String companysave(@ModelAttribute HostCompanyregVo companyregVo
-							 ) {
+	public String companysave(@ModelAttribute HostCompanyregVo cVo,
+							  @RequestParam("phone1") String phone1,
+							  @RequestParam("phone2") String phone2,
+							  @RequestParam("phone3") String phone3,
+							  @RequestParam("tel1") String tel1,
+							  @RequestParam("tel2") String tel2,
+							  @RequestParam("tel3") String tel3
+							  ) {
+		
+		cregService.insertCompany(cVo, phone1, phone2, phone3, tel1, tel2, tel3);
+
+		return "redirect:/host/companyreg";
+	}
+	
+	/*
+	@RequestMapping(value="updatecompany", method = {RequestMethod.GET, RequestMethod.POST})
+	public String updatecompany(Model model,
+								@ModelAttribute HostCompanyregVo cVo,
+			  					@RequestParam("phone1") String phone1,
+	  							@RequestParam("phone2") String phone2,
+	  							@RequestParam("phone3") String phone3,
+	  							@RequestParam("tel1") String tel1,
+	  							@RequestParam("tel2") String tel2
+	  							@RequestParam("tel3") String tel3) {
 		return "redirect:/host/UpdatecompanyRegister";
 	}
 	*/
--- src/main/java/com/javaex/service/HostCompanyregService.java
+++ src/main/java/com/javaex/service/HostCompanyregService.java
@@ -19,4 +19,42 @@ public class HostCompanyregService {
 	}
 	
 	
+	public int insertCompany(HostCompanyregVo cVo, String phone1, String phone2, String phone3, String tel1, String tel2, String tel3) {
+		
+		String ceoNo = phone1 + "-" + phone2 + "-" + phone3;
+		String companyNo = tel1 + "-" + tel2 + "-" + tel3;
+		String removeNull = "";
+		
+		cVo.setCeoHp(ceoNo);
+		cVo.setCompanyHp(companyNo);
+		
+		if(cVo.getBankName().equals("없음")) {
+			cVo.setBankName(removeNull);
+			cVo.setAccountHolder(removeNull);
+			cVo.setAccount(removeNull);
+			cVo.setSettlementCycle(removeNull);
+		}
+		
+		if(cVo.getTaxInvoice() == 1) {
+			cVo.setCompanyName(removeNull);
+			cVo.setCeoName(removeNull);
+			cVo.setBusinessLicensenumber(0);
+			cVo.setPostalCode(removeNull);
+			cVo.setCompanyAddress(removeNull);
+			cVo.setDetailAddress(removeNull);
+			cVo.setTaxInvoiceEmail(removeNull);
+		}
+		
+		if(cVo.getHomepage() == null) {
+			cVo.setHomepage(removeNull);
+		}
+		
+		System.out.println(cVo);
+		
+		int count = cregDao.insertCompany(cVo);
+		
+		return count;
+	}
+	
+	
 }
