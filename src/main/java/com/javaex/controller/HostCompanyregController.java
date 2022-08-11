a672214c011f2957b35e2465989d79ab3fe957f7
Mocking_jayk0918 <jesper2able@gmail.com>
2022-08-11 12:19:19 +0900

    업주컨트롤러 수

--- src/main/java/com/javaex/controller/HostCompanyregController.java
+++ src/main/java/com/javaex/controller/HostCompanyregController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.service.HostCompanyregService;
 import com.javaex.vo.HostCompanyregVo;
@@ -29,31 +28,18 @@ public class HostCompanyregController {
 	}
 
 	@RequestMapping(value="companysave", method = {RequestMethod.GET, RequestMethod.POST})
-	public String companysave(@ModelAttribute HostCompanyregVo cVo,
-							  @RequestParam("phone1") String phone1,
-							  @RequestParam("phone2") String phone2,
-							  @RequestParam("phone3") String phone3,
-							  @RequestParam("tel1") String tel1,
-							  @RequestParam("tel2") String tel2,
-							  @RequestParam("tel3") String tel3
-							  ) {
-		
-		cregService.insertCompany(cVo, phone1, phone2, phone3, tel1, tel2, tel3);
-
+	public String companysave(@ModelAttribute HostCompanyregVo cVo) {
+		System.out.println(cVo);
+		cregService.insertCompany(cVo);
 		return "redirect:/host/companyreg";
 	}
 	
 	/*
 	@RequestMapping(value="updatecompany", method = {RequestMethod.GET, RequestMethod.POST})
 	public String updatecompany(Model model,
-								@ModelAttribute HostCompanyregVo cVo,
-			  					@RequestParam("phone1") String phone1,
-	  							@RequestParam("phone2") String phone2,
-	  							@RequestParam("phone3") String phone3,
-	  							@RequestParam("tel1") String tel1,
-	  							@RequestParam("tel2") String tel2
-	  							@RequestParam("tel3") String tel3) {
+								@ModelAttribute HostCompanyregVo cVo) {
 		return "redirect:/host/UpdatecompanyRegister";
 	}
 	*/
+	
 }
