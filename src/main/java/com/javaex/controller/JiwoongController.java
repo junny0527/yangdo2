dfe05c666da8347316bf527e48cc55c1064cacfa
user <user@192.168.219.104>
2022-08-12 15:50:57 +0900

    펜션 리스트 --> 정보 불러오기

--- src/main/java/com/javaex/controller/JiwoongController.java
+++ src/main/java/com/javaex/controller/JiwoongController.java
@@ -1,5 +1,6 @@
 package com.javaex.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.service.MainListService;
 
@@ -17,21 +19,24 @@ public class JiwoongController {
 	@Autowired
 	private MainListService mainService;
 	
-	@RequestMapping(value="/main2", method={RequestMethod.GET, RequestMethod.POST})
-	public String mainList(Model model, @RequestParam("penNo") int penNo) {
+	@RequestMapping(value="/main", method={RequestMethod.GET, RequestMethod.POST})
+	public String mainList(Model model, @RequestParam("areaNo") int areaNo,
+										@RequestParam(value = "crtPage", required = false, defaultValue = "1") int crtPage) {
 		System.out.println("jiwoongController>mainList");
+		System.out.println(areaNo);
+		Map<String, Object> penMapList = mainService.select(areaNo);
 		
-		Map<String, Object> penMap = mainService.select(penNo);
-		System.out.println(penMap);
-		System.out.println(penNo);
+		System.out.println(penMapList);
+		
+		//펜션 정보 
+		model.addAttribute("penMapList",penMapList);
+		
+		//사진 가져오기
+		
+		
 		return "/mainList/mainLists";
 	}
 	
-	@RequestMapping(value="/main", method={RequestMethod.GET, RequestMethod.POST})
-	public String mainList() {
-		System.out.println("jiwoongController>mainList");
-		
-		return "/mainList/mainLists";
-	}
+	
 	
 }
--- src/main/java/com/javaex/dao/MainListDao.java
+++ src/main/java/com/javaex/dao/MainListDao.java
@@ -1,20 +1,30 @@
 package com.javaex.dao;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.PensionImageVo;
 
 @Repository
 public class MainListDao {
-	
+	@Autowired
 	private SqlSession sqlSession;
 	
-	public Map<String, Object> select(int penNo){
+	// 펜션정보
+	public Map<String, Object> select(int areaNo){
+		System.out.println("mainDao>select");
 		
-		Map<String, Object> pensionMap = sqlSession.selectOne("MainList.penSelect", penNo);
+		Map<String, Object> penMapList =  sqlSession.selectOne("MainList.penSelect", areaNo);
+		System.out.println(penMapList);
 		
-		return pensionMap;
+		return penMapList;
 	}
+	
+		
 	
 }
