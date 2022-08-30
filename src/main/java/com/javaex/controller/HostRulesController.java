eadc52c3d5f0ac9a5d8cd3cd6c191897b2344c8b
Mocking_jayk0918 <jesper2able@gmail.com>
2022-08-30 12:00:27 +0900

    업주 이용규칙 컨트롤러 롤백다시

--- src/main/java/com/javaex/controller/HostRulesController.java
+++ src/main/java/com/javaex/controller/HostRulesController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,18 +24,7 @@ public class HostRulesController {
 	private HostIntroduceService iService;
 	
 	@RequestMapping(value="rules", method= {RequestMethod.GET, RequestMethod.POST})
-	public String rules(HttpSession session, Model model) {
-		UserVo uVo = (UserVo) session.getAttribute("authUser");
-		int userNo = uVo.getNo();
-		int pensionNo = iService.getPensionNoBySession(userNo);
-		HostRulesVo rInfo = rService.getRules(pensionNo);
-		//List<HostRulesVo> peck = rService.getPeck(pensionNo);
-		//List<HostRulesVo> subpeck = rService.getsubPeck(pensionNo);
-		
-		model.addAttribute("rInfo", rInfo);
-		//model.addAttribute("peck", peck);
-		//model.addAttribute("subpeck", subpeck);
-		
+	public String rules() {
 		return "/host/rules";
 	}
 	
