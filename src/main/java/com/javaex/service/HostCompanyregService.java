b8fb42cd2301c528f9679bd0bdc620e046594cde
Mocking_jayk0918 <jesper2able@gmail.com>
2022-08-30 15:21:54 +0900

    업주 이상한 전화번호 가져오기 고침

--- src/main/java/com/javaex/service/HostCompanyregService.java
+++ src/main/java/com/javaex/service/HostCompanyregService.java
@@ -81,41 +81,16 @@ public class HostCompanyregService {
 		HostCompanyregVo oldCompany = cregDao.getCompanyinfo(userNo);
 		oldCompany.setEmail(email);
 		
-		String[] phoneSplit = oldCompany.getCeoHp().split("");
-		String[] companySplit = oldCompany.getCompanyHp().split("");
+		String[] phoneSplit = oldCompany.getCeoHp().split("-");
+		String[] companySplit = oldCompany.getCompanyHp().split("-");
 		
-		String phoneIdentify = phoneSplit[0] + phoneSplit[1] + phoneSplit[2];
-		oldCompany.setPhone1(phoneIdentify);
-		
-		if(phoneIdentify.equals("010")) {
-			oldCompany.setPhone2(phoneSplit[3] + phoneSplit[4] + phoneSplit[5] + phoneSplit[6]);
-			oldCompany.setPhone3(phoneSplit[7] + phoneSplit[8] + phoneSplit[9] + phoneSplit[10]);
-		}else {
-			oldCompany.setPhone2(phoneSplit[3] + phoneSplit[4] + phoneSplit[5]);
-			oldCompany.setPhone2(phoneSplit[6] + phoneSplit[7] + phoneSplit[8] + phoneSplit[9]);
-		}
-		
-		int splitLength = companySplit.length;
-		
-		if(splitLength == 9) {
-			oldCompany.setTel1(companySplit[0] + companySplit[1]);
-			oldCompany.setTel2(companySplit[2] + companySplit[3] + companySplit[4]);
-			oldCompany.setTel3(companySplit[5] + companySplit[6] + companySplit[7]);
-		}else if(splitLength == 12) {
-			oldCompany.setTel1(companySplit[0] + companySplit[1] + companySplit[2] + companySplit[3]);
-			oldCompany.setTel2(companySplit[4] + companySplit[5] + companySplit[6] + companySplit[7]);
-			oldCompany.setTel3(companySplit[8] + companySplit[9] + companySplit[10] + companySplit[11]);
-		}else {
-			if((companySplit[0] + companySplit[1]).equals("02")) {
-				oldCompany.setTel1(companySplit[0] + companySplit[1]);
-				oldCompany.setTel2(companySplit[2] + companySplit[3] + companySplit[4] + companySplit[5]);
-				oldCompany.setTel3(companySplit[6] + companySplit[7] + companySplit[8] + companySplit[9]);
-			}else {
-				oldCompany.setTel1(companySplit[0] + companySplit[1] + companySplit[2]);
-				oldCompany.setTel2(companySplit[3] + companySplit[4] + companySplit[5]);
-				oldCompany.setTel3(companySplit[6] + companySplit[7] + companySplit[8] + companySplit[9]);
-			}
-		}
+		System.out.println(phoneSplit[0] + "+" + phoneSplit[1] + "+" +  phoneSplit[2]);
+		oldCompany.setPhone1(phoneSplit[0]);
+		oldCompany.setPhone2(phoneSplit[1]);
+		oldCompany.setPhone3(phoneSplit[2]);
+		oldCompany.setTel1(companySplit[0]);
+		oldCompany.setTel2(companySplit[1]);
+		oldCompany.setTel3(companySplit[2]);
 		
 		int companyNo = cregDao.getCompanyNoBySession(userNo);
 		System.out.println("list service : " + companyNo);
